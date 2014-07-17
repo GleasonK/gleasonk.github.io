9fa5e9eba3038e8c31c3a7b9c6d0b5dac3998514
GleasonK <kaagleason@gmail.com>
2014-07-17 11:28:56 -0500

    Made multi-browser friendly

--- js/main.js
+++ js/main.js
@@ -21,19 +21,21 @@ $(document).ready(function(){
 		 'margin-top' : mTop + 'px',
 		 'opacity': 1+topPos/300 
 		});
-		$('#banner-wave.wave1').css('background-position-x', waveSpeed/10 + 'px');
-		$('#banner-wave.wave2').css('background-position-x',-25 + -waveSpeed/7 + 'px');
-		$('#banner-wave.wave3').css('background-position-x', -50  -waveSpeed/2.5 + 'px');
+		$('#banner-wave.wave1').css('background-position', waveSpeed/10 + 'px 0');
+		$('#banner-wave.wave2').css('background-position',-25 + -waveSpeed/7 + 'px 0');
+		$('#banner-wave.wave3').css('background-position', -50  -waveSpeed/2.5 + 'px 0');
 
 		var dayDegree = 180 + -daylightSpeed;
 		var nightDegree = -50 + - nightlightSpeed;
 		if (bottomOfScreen < (-dayPos) && dayDegree < 440) {
-			$('#hemisphere.sun-orbit').css('-webkit-transform', 'rotate(' + dayDegree + 'deg)');
-			$('#globe').css('-webkit-transform', 'rotate(' + (60 + dayDegree) + 'deg)');
+			transformAll($('#hemisphere.sun-orbit'), dayDegree);
+			transformAll($('#globe'), (60+dayDegree));
+			// $('#hemisphere.sun-orbit').css('-webkit-transform', 'rotate(' + dayDegree + 'deg)');
+			// $('#globe').css('-webkit-transform', 'rotate(' + (60 + dayDegree) + 'deg)');
 		}
 		if (bottomOfScreen < (-nightPos) && nightDegree < 435){
-			$('#hemisphere.moon-orbit').css('-webkit-transform', 'rotate(' + (nightDegree) + 'deg)');
-/* 			alert(nightDegree); */
+			transformAll($('#hemisphere.moon-orbit'), nightDegree);
+			// $('#hemisphere.moon-orbit').css('-webkit-transform', 'rotate(' + (nightDegree) + 'deg)');
 		}
 	})
 });
@@ -50,6 +52,7 @@ $(document).ready(function(){
     var beginning_color_r =  new $.Color( 'rgb(255, 255, 0)' ); //Yellow
     var ending_color = new $.Color( 'rgb(0, 179, 227)' );   //Blue 
     var ending_color_r = new $.Color( 'rgb(115, 0, 255)' ); //Purple
+	gradientAll($('#bg-gradient'), beginning_color, beginning_color_r);
     $(document).scroll(function() {
         scroll_pos = $(this).scrollTop(); 
         if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos ) { 
@@ -63,11 +66,14 @@ $(document).ready(function(){
             var newGreen_r = beginning_color_r.green() + ( ( ending_color_r.green() - beginning_color_r.green() ) * percentScrolled );
             var newBlue_r = beginning_color_r.blue() + ( ( ending_color_r.blue() - beginning_color_r.blue() ) * percentScrolled );
             var newColor_r = new $.Color( newRed_r, newGreen_r, newBlue_r );
-            $('#bg-gradient').css({ background: "-webkit-gradient(linear, left top, right bottom, from("+ newColor +"), to("+ newColor_r +"))"});
+            gradientAll($('#bg-gradient'), newColor, newColor_r);
+            // $('#bg-gradient').css({ background: "-webkit-gradient(linear, left top, right bottom, from("+ newColor +"), to("+ newColor_r +"))"});
         } else if ( scroll_pos > animation_end_pos ) {
-             $('#bg-gradient').css({ background: "-webkit-gradient(linear, left top, right bottom, from("+ ending_color +"), to("+ ending_color_r +"))"});
+            gradientAll($('#bg-gradient'), ending_color, ending_color_r);
+             // $('#bg-gradient').css({ background: "-webkit-gradient(linear, left top, right bottom, from("+ ending_color +"), to("+ ending_color_r +"))"});
         } else if ( scroll_pos < animation_begin_pos ) {
-             $('#bg-gradient').css({ background: "-webkit-gradient(linear, left top, right bottom, from("+ beginning_color +"), to("+ beginning_color_r +"))"});
+            gradientAll($('#bg-gradient'), beginning_color, beginning_color_r);
+             // $('#bg-gradient').css({ background: "-webkit-gradient(linear, left top, right bottom, from("+ beginning_color +"), to("+ beginning_color_r +"))"});
         } else { } 
     });
 });
