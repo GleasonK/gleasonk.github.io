cabfaa32933d1ff35c5bbf35ef541434b3f49706
Kevin Gleason <kaagleason@gmail.com>
2014-08-01 23:40:05 -0500

    Revamped

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@
 $(window).load(function(){
 	$('<img/>').attr('src', 'Content/Fifty.svg').load(function() {
 	   $(this).remove(); // prevent memory leaks as @benweet suggested
-	   $('body').css('background-image', 'url(Content/Fifty.svg)');
 	});
+	$('body').css('background-image', 'url(Content/Fifty.svg)');
+	show();
 });
 
+function show(){
+	$('#loading').delay(20000).hide()
+	$("#container").fadeIn("slow");	
+}
+
 $(document).ready(function() {
-   $(".coming-soon").fadeIn();
+   $(".coming-soon").fadeIn("slow");
 });
