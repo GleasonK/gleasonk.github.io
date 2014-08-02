0ea06bdf56b5594d9b01d93aaafff6a1250657aa
Kevin Gleason <kaagleason@gmail.com>
2014-08-02 00:26:23 -0500

    Revamped

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 // window.history.pushState("object or string", "Title", "/lol");
 $(window).load(function(){
-	$('<img/>').attr('src', 'Content/Fifty.svg').load(function() {
-	   $(this).remove(); // prevent memory leaks as @benweet suggested
-	});
-	$('body').css('background-image', 'url(Content/Fifty.svg)');
-	show();
+	// $('<img/>').attr('src', 'Content/Fifty.svg').load(function() {
+	   // $(this).remove(); // prevent memory leaks as @benweet suggested
+	// });
+	// $('body').css('background-image', 'url(Content/Fifty.svg)');
+	show()
 });
 
 function show(){
 	var dtime = 2000
-	$('#loading').hide('fast').fadeOut(
+	$('#loading').delay(250).fadeOut(1000,
 		function(){
-			$("#container").removeClass("gone").fadeIn(2500);	
+			$("#container").removeClass("gone").fadeIn(1500, 
+				function(){
+					var cb = $('#creators-box');
+					cb.css('bottom',-100);
+
+					$('#creators-box').stop().animate({ bottom:'+=100px' }, "slow")
+				});	
 	});
 }
